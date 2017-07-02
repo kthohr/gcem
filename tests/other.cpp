89db755aa500c3f38da6230e044171fd7890be23
Keith O'Hara <kthohr@users.noreply.github.com>
2017-07-02 21:57:03 +0100

    add test to increase coverage

---
+++ tests/other.cpp
@@ -0,0 +1,33 @@
+/*################################################################################
+  ##
+  ##   Copyright (C) 2016-2017 Keith O'Hara
+  ##
+  ##   This file is part of the GCE-Math C++ library.
+  ##
+  ##   GCE-Math is free software: you can redistribute it and/or modify
+  ##   it under the terms of the GNU General Public License as published by
+  ##   the Free Software Foundation, either version 2 of the License, or
+  ##   (at your option) any later version.
+  ##
+  ##   GCE-Math is distributed in the hope that it will be useful,
+  ##   but WITHOUT ANY WARRANTY; without even the implied warranty of
+  ##   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  ##   GNU General Public License for more details.
+  ##
+  ################################################################################*/
+
+// g++-mp-7 -O3 -Wall -std=c++11 -I./../include -I/opt/local/include other.cpp -o other.test -framework Accelerate
+
+#include <iostream>
+#include "gcem.hpp"
+
+int main()
+{
+    double x = 1.5;
+    
+    gcem::sign_dbl(x);
+    gcem::find_fraction(x);
+    gcem::find_whole(x);
+
+    return 0;
+}
