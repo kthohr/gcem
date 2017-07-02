6dc28a0f78eefe9131f639c633bee7fb5bac78ab
Keith O'Hara <kthohr@users.noreply.github.com>
2017-07-02 18:42:30 +0100

    update test for gcov

--- tests/binomial_coef.cpp
+++ tests/binomial_coef.cpp
@@ -28,11 +28,11 @@ int main()
     constexpr int n = 5;
     constexpr int k = 2;
 
-    constexpr long double result = gcem::binomial_coef(n,k);
+    // long double result = gcem::binomial_coef(n,k);
 
     std::cout << "\nbegin binomial_coef test\n" << std::endl;
     
-    std::cout << std::setiosflags(std::ios::fixed) << std::setprecision(6) << "gcem_binomial_coef(" << n <<"," << k << ") = " << std::setprecision(18) << result << std::endl;
+    std::cout << std::setiosflags(std::ios::fixed) << std::setprecision(6) << "gcem_binomial_coef(" << n <<"," << k << ") = " << std::setprecision(2) << gcem::binomial_coef(n,k) << std::endl;
 
     std::cout << "\nend binomial_coef test" << std::endl;
 
